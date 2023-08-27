4a2ab766cdf2b1eef124c409957b10f59ea486ab
Luciano Briganti <luciano.briganti@wincasa.ch>
2023-08-27 12:19:15 +0200

    add test output helper

--- Kata.FizzBuzz/Kata.FizzBuzz/Test/1. MainMission/FizzBuzzSagaShould.cs
+++ Kata.FizzBuzz/Kata.FizzBuzz/Test/1. MainMission/FizzBuzzSagaShould.cs
@@ -1,12 +1,20 @@
 using Shouldly;
+using Xunit.Abstractions;
 
 namespace Kata.FizzBuzz.Test._1._MainMission
 {
     public class FizzBuzzSagaShould
     {
-        /*
+		/*
          * Test your spells using TDD approach.
          */
+		
+        private readonly ITestOutputHelper _outputHelper;
+
+        public FizzBuzzSagaShould(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
 
         [Fact]
         public async Task TestFoo_Withxx_Expectxx()
